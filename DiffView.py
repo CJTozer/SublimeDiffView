8f1de0bc15e5a1ce00914c5bb102370d83397f0f
Chris Tozer <chris.tozer@metaswitch.com>
2015-12-09 22:14:16 +0000

    Parse "diff --stat" command

--- DiffView.py
+++ DiffView.py
@@ -1,5 +1,7 @@
 import sublime
 import sublime_plugin
+import subprocess
+import re
 
 class DiffView(sublime_plugin.WindowCommand):
     last_diff = ''
@@ -11,5 +13,30 @@ class DiffView(sublime_plugin.WindowCommand):
         self.window.show_input_panel("Diff parameters?", self.last_diff, self.do_diff, None, None)
 
     def do_diff(self, diff_args):
+        if diff_args == '':
+            diff_args = 'HEAD'
         print("Diff args: %s" % diff_args)
         self.last_diff = diff_args
+
+        parser = DiffParser(diff_args)
+
+        for f in parser.changed_files():
+            print("File {} has changed".format(f))
+
+class DiffParser(object):
+    STAT_CHANGED_FILE = re.compile('\s*([\w\.]+)\s*\|')
+
+    def __init__(self, diff_args):
+        self.diff_args = diff_args
+
+    def changed_files(self):
+        for line in git_command(['diff', '--stat', self.diff_args]).split('\n'):
+            match = self.STAT_CHANGED_FILE.match(line)
+            if match:
+                yield match.group(1)
+
+def git_command(args):
+    p = subprocess.Popen(['git'] + args,
+                         stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    return out.decode('utf-8')
