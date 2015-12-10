afcf3bb7210995569852e260656dfb237b903709
Chris Tozer <chris.tozer@metaswitch.com>
2015-12-10 00:16:51 +0000

    Hunk parsing.  Need to handle mid-line changes better with 'porcelain' diff formatting

--- DiffView.py
+++ DiffView.py
@@ -19,11 +19,13 @@ class DiffView(sublime_plugin.WindowCommand):
 
         parser = DiffParser(diff_args)
 
+        # For now, just print some info...
         for f in parser.changed_files:
             print("File {} has changed".format(f.filename))
             f.parse_diff()
             for hunk in f.hunks:
                 print(hunk.hunk_match.group(0))
+                hunk.parse_diff()
 
 class DiffFilesList(sublime_plugin.WindowCommand):
     def run(self):
@@ -42,10 +44,10 @@ class DiffParser(object):
         for line in git_command(['diff', '--stat', self.diff_args]).split('\n'):
             match = self.STAT_CHANGED_FILE.match(line)
             if match:
-                files.append(FileDiffParser(match.group(1), self.diff_args))
+                files.append(FileDiff(match.group(1), self.diff_args))
         return files
 
-class FileDiffParser(object):
+class FileDiff(object):
     HUNK_MATCH = re.compile('^@@ \-(\d+),(\d+) \+(\d+),(\d+) @@')
 
     def __init__(self, filename, diff_args):
@@ -56,20 +58,61 @@ class FileDiffParser(object):
 
     def parse_diff(self):
         if not self.diff_text:
-            self.diff_text = git_command(['diff', self.diff_args, '--', self.filename])
+            self.diff_text = git_command(['diff', self.diff_args, '--minimal', '--word-diff=porcelain', '--', self.filename])
             hunk = None
             for line in self.diff_text.split('\n'):
                 match = self.HUNK_MATCH.match(line)
                 if match:
-                    hunk = HunkDiffParser(match)
+                    hunk = HunkDiff(match)
                     self.hunks.append(hunk)
                 elif hunk:
-                    hunk.diff_lines.append(line)
+                    hunk.diff_text_lines.append(line)
 
-class HunkDiffParser(object):
+class HunkDiff(object):
+    LINE_DELIM_MATCH = re.compile('^~')
+    ADD_LINE_MATCH = re.compile('^\+(.*)')
+    DEL_LINE_MATCH = re.compile('^\-(.*)')
+
     def __init__(self, hunk_match):
         self.hunk_match = hunk_match
-        self.diff_lines = []
+        self.diff_text_lines = []
+        self.line_diffs = []
+
+    def parse_diff(self):
+        old_line_num = int(self.hunk_match.group(1)) - 1
+        new_line_num = int(self.hunk_match.group(3)) - 1
+        old_line = ''
+        new_line = ''
+
+        for line in self.diff_text_lines:
+            delim_match = self.LINE_DELIM_MATCH.match(line)
+            add_match = self.ADD_LINE_MATCH.match(line)
+            del_match = self.DEL_LINE_MATCH.match(line)
+
+            if delim_match:
+                if old_line or new_line:
+                    self.line_diffs.append(LineDiff(old_line_num, old_line, new_line_num, new_line))
+                if not old_line and not new_line:
+                    old_line_num += 1
+                    new_line_num += 1
+                old_line = ''
+                new_line = ''
+            elif add_match:
+                new_line_num += 1
+                new_line = add_match.group(1)
+            elif del_match:
+                old_line_num += 1
+                old_line = del_match.group(1)
+
+class LineDiff(object):
+    def __init__(self, old_line_number, old_line, new_line_number, new_line):
+        self.old_line_number = old_line_number
+        self.old_line = old_line
+        self.new_line_number = new_line_number
+        self.new_line = new_line
+        print("Creating LineDiff with:")
+        print("  Old line ({}): {}".format(old_line_number, old_line))
+        print("  New line ({}): {}".format(new_line_number, new_line))
 
 def git_command(args):
     p = subprocess.Popen(['git'] + args,
