2a5b4d374621ad8ec05c5801ab8744fdd11e98f0
Chris Tozer <chris.tozer@metaswitch.com>
2015-12-12 15:55:25 +0000

    Hunk-by-hunk navigation

--- DiffView.py
+++ DiffView.py
@@ -11,13 +11,9 @@ class DiffView(sublime_plugin.WindowCommand):
     diff_base = ''
 
     def run(self):
-        self.git_base = git_command(['rev-parse', '--show-toplevel']).rstrip()
         self.window.last_diff = self
-        self.last_file_index = 0
+        self.last_hunk_index = 0
         self.preview = None
-
-        # Create a temporary directory for (read-only) old versions of changed files
-        self.temp_dir = tempfile.mkdtemp()
 
         # Use show_input_panel as show_quick_panel doesn't allow arbitrary data
         self.window.show_input_panel("Diff against? [HEAD]", self.diff_base, self.do_diff, None, None)
@@ -33,97 +29,77 @@ class DiffView(sublime_plugin.WindowCommand):
         # Create the diff parser
         self.parser = DiffParser(self.diff_base)
 
-        # Create the old files in the temporary directory
-        self.create_old_files()
+        # Show the list of changed hunks
+        self.list_changed_hunks()
 
-        # Show the list of changed files
-        self.list_changed_files()
-
-    def create_old_files(self):
-        for changed_file in self.parser.changed_files:
-            target_dir = os.path.join(self.temp_dir, os.path.dirname(changed_file.filename))
-            changed_file.old_file = os.path.join(self.temp_dir, changed_file.filename)
-            if not os.path.exists(target_dir):
-                os.makedirs(os.path.join(self.temp_dir, os.path.dirname(changed_file.filename)))
-            with open(changed_file.old_file, 'w') as f:
-                git_args = ['show', '{}:{}'.format(self.diff_base, changed_file.filename)]
-                old_file_content = git_command(git_args)
-                f.write(old_file_content.replace('\r\n', '\n'))
-
-    def list_changed_files(self):
-        print(self.parser.changed_files)
+    def list_changed_hunks(self):
         self.window.show_quick_panel(
-            [f.filename for f in self.parser.changed_files],
-            self.show_file_diff,
+            [h.description for h in self.parser.changed_hunks],
+            self.show_hunk_diff,
             0,
-            self.last_file_index,
-            self.preview_diff)
+            self.last_hunk_index,
+            self.preview_hunk)
 
-    def show_file_diff(self, index):
-        print("show_file_diff: {}".format(index))
+    def show_hunk_diff(self, index):
+        print("show_hunk_diff: {}".format(index))
         if index == -1:
-            # Reset the layout
-            self.preview_old_file.close()
-            sublime.active_window().set_layout(self.orig_layout)
+            if self.preview:
+                self.preview.close()
+                self.preview = None
             return
+        self.last_hunk_index = index
+        hunk = self.parser.changed_hunks[index]
+        self.window.open_file(hunk.filespec(), sublime.ENCODED_POSITION)
 
-        self.last_file_index = index
-        changed_file = self.parser.changed_files[index]
-        self.window.open_file(changed_file.old_file)
-        # sublime.ENCODED_POSITION flag will look for "file:line:col", which will be useful later.
+    def preview_hunk(self, index):
+        hunk = self.parser.changed_hunks[index]
+        already_exists = self.window.find_open_file(hunk.file_diff.filename)
+        self.preview = self.window.open_file(hunk.filespec(), sublime.TRANSIENT | sublime.ENCODED_POSITION)
+        if already_exists:
+            self.preview = None
 
 
-    def preview_diff(self, index):
-        changed_file = self.parser.changed_files[index]
-
-        self.window.set_layout( { "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]] } )
-        #new_file_abs = os.path.join(self.git_base, changed_file.filename)
-        #self.preview_new_file = self.window.open_file(new_file_abs, sublime.TRANSIENT)
-        self.preview_old_file = self.window.open_file(changed_file.old_file)#, sublime.TRANSIENT)
-
-        # Wait for the view to load
-        def move_old_view():
-            print("Starting THREAD")
-            while self.preview_old_file.is_loading():
-                time.sleep(0.1)
-            print("Moving view")
-            print(self.window.active_group())
-            self.window.set_view_index(self.preview_old_file, 1, 0)
-
-        threading.Thread(target=move_old_view).start()
-
-        #self.window.set_view_index(self.preview_new_file, 1, 0)
-        # sublime.ENCODED_POSITION flag will look for "file:line:col", which will be useful later.
-
-class DiffFilesList(sublime_plugin.WindowCommand):
+class DiffHunksList(sublime_plugin.WindowCommand):
     def run(self):
         if hasattr(self.window, 'last_diff'):
-            self.window.last_diff.list_changed_files()
+            self.window.last_diff.list_changed_hunks()
 
 class DiffParser(object):
     STAT_CHANGED_FILE = re.compile('\s*([\w\.\-\/]+)\s*\|')
 
     def __init__(self, diff_base):
+        self.git_base = git_command(['rev-parse', '--show-toplevel']).rstrip()
         self.diff_base = diff_base
         self.changed_files = self._get_changed_files()
+        self.changed_hunks = []
+        for f in self.changed_files:
+            self.changed_hunks += f.get_hunks()
 
     def _get_changed_files(self):
         files = []
         for line in git_command(['diff', '--stat', self.diff_base]).split('\n'):
             match = self.STAT_CHANGED_FILE.match(line)
             if match:
-                files.append(FileDiff(match.group(1), self.diff_base))
+                filename = match.group(1)
+                abs_filename = os.path.join(self.git_base, filename)
+                files.append(FileDiff(match.group(1), abs_filename, self.diff_base))
         return files
 
 class FileDiff(object):
     HUNK_MATCH = re.compile('\r?\n@@ \-(\d+),\d+ \+(\d+),\d+ @@')
 
-    def __init__(self, filename, diff_args):
+    def __init__(self, filename, abs_filename, diff_args):
         self.filename = filename
+        self.abs_filename = abs_filename
         self.old_file = 'UNDEFINED'
         self.diff_args = diff_args
         self.diff_text = ''
         self.hunks = []
+
+    def get_hunks(self):
+        if not self.hunks:
+            self.parse_diff()
+        return self.hunks
 
     def parse_diff(self):
         if not self.diff_text:
@@ -134,7 +110,8 @@ class FileDiff(object):
             print(hunks)
             while len(hunks) >= 3:
                 # Don't force all parsing up-front
-                self.hunks.append(HunkDiff(old_line_num=hunks[0],
+                self.hunks.append(HunkDiff(self,
+                                           old_line_num=hunks[0],
                                            new_line_num=hunks[1],
                                            hunk_diff_text=hunks[2]))
                 hunks = hunks[3:]
@@ -144,10 +121,12 @@ class HunkDiff(object):
     ADD_LINE_MATCH = re.compile('^\+(.*)')
     DEL_LINE_MATCH = re.compile('^\-(.*)')
 
-    def __init__(self, old_line_num, new_line_num, hunk_diff_text):
+    def __init__(self, file_diff, old_line_num, new_line_num, hunk_diff_text):
+        self.file_diff = file_diff
         self.old_line_num = int(old_line_num)
         self.new_line_num = int(new_line_num)
         self.hunk_diff_text = hunk_diff_text
+        self.description = "{}:{}".format(file_diff.filename, self.new_line_num)
         print("Created new hunk.")
         print("    Old line start: {}".format(self.old_line_num))
         print("    New line start: {}".format(self.new_line_num))
@@ -162,6 +141,9 @@ class HunkDiff(object):
             print("$$$$" + region + "&&&&")
             add_match = self.ADD_LINE_MATCH.match(region)
             del_match = self.DEL_LINE_MATCH.match(region)
+
+    def filespec(self):
+        return "{}:{}".format(self.file_diff.abs_filename, self.new_line_num)
 
 def git_command(args):
     p = subprocess.Popen(['git'] + args,
