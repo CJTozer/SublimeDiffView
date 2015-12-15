c3400ba5009ac7fbb6a42fc3861b17e7766f314c
Chris Tozer <christopherjtozer@gmail.com>
2015-12-15 10:20:10 +0000

    View old file on left

--- DiffView.py
+++ DiffView.py
@@ -1,8 +1,9 @@
 import sublime
 import sublime_plugin
-import subprocess
-import re
 import os
+import re
+import subprocess
+import tempfile
 
 ADD_REGION_KEY = 'diffview-highlight-addition'
 MOD_REGION_KEY = 'diffview-highlight-modification'
@@ -20,6 +21,7 @@ class DiffView(sublime_plugin.WindowCommand):
     def run(self):
         self.window.last_diff = self
         self.last_hunk_index = 0
+        self.temp_dir = tempfile.mkdtemp()
 
         # Use show_input_panel as show_quick_panel doesn't allow arbitrary data
         self.window.show_input_panel(
@@ -43,12 +45,44 @@ class DiffView(sublime_plugin.WindowCommand):
         cwd = os.path.dirname(self.window.active_view().file_name())
         self.parser = DiffParser(self.diff_args, cwd)
 
+        # Create the required temporary files
+        self.create_files()
+
         if not self.parser.changed_hunks:
             # No changes; say so
             sublime.message_dialog("No changes to report...")
         else:
             # Show the list of changed hunks
             self.list_changed_hunks()
+
+    def create_files(self):
+        """Create all the files needed to show the diffs."""
+        for changed_file in self.parser.changed_files:
+            changed_file.old_file = os.path.join(
+                self.temp_dir,
+                'old',
+                changed_file.filename)
+            old_dir = os.path.dirname(changed_file.old_file)
+
+            if not os.path.exists(old_dir):
+                os.makedirs(old_dir)
+            with open(changed_file.old_file, 'w') as f:
+                git_args = [
+                    'show',
+                    '{}:{}'.format(self.diff_args, changed_file.filename)]
+                old_file_content = git_command(git_args, self.parser.git_base)
+                f.write(old_file_content.replace('\r\n', '\n'))
+
+            # TODO - when doing more complex diffs, need to grab a blob with
+            # `git show` like above, and copy to the 'new' temporary directory.
+            # changed_file.new_file = os.path.join(
+            #     self.temp_dir,
+            #     'new',
+            #     changed_file.filename)
+            # new_dir = os.path.dirname(changed_file.new_file)
+            # if not os.path.exists(new_dir):
+            #     os.makedirs(new_dir)
+            # shutil.copyfile(changed_file.abs_filename, changed_file.new_file)
 
     def list_changed_hunks(self):
         """Show a list of changed hunks in a quick panel."""
@@ -102,7 +136,8 @@ class DiffView(sublime_plugin.WindowCommand):
 
         self.last_hunk_index = hunk_index
         hunk = self.parser.changed_hunks[hunk_index]
-        self.window.open_file(hunk.filespec(), sublime.ENCODED_POSITION)
+        (_, new_filespec) = hunk.filespecs()
+        self.window.open_file(new_filespec, sublime.ENCODED_POSITION)
 
     def preview_hunk(self, hunk_index):
         """Show a preview of the selected hunk.
@@ -111,13 +146,21 @@ class DiffView(sublime_plugin.WindowCommand):
             hunk_index: the selected index in the changed hunks list.
         """
         hunk = self.parser.changed_hunks[hunk_index]
+        (old_filespec, new_filespec) = hunk.filespecs()
         right_view = self.window.open_file(
-            hunk.filespec(),
+            new_filespec,
             flags=sublime.TRANSIENT |
             sublime.ENCODED_POSITION |
             sublime.FORCE_GROUP,
             group=1)
         hunk.file_diff.add_regions(right_view)
+
+        left_view = self.window.open_file(
+            old_filespec,
+            flags=sublime.TRANSIENT |
+            sublime.ENCODED_POSITION |
+            sublime.FORCE_GROUP,
+            group=0)
 
         # Keep the focus in the quick panel
         self.window.focus_group(0)
@@ -395,11 +438,17 @@ class HunkDiff(object):
 
         return (add_chunks, del_chunks)
 
-    def filespec(self):
+    def filespecs(self):
         """Get the portion of code that this hunk refers to in the format
-        `filename:col`.
+        `(old_filename:old_line, new_filename:new_line`.
         """
-        return "{}:{}".format(self.file_diff.abs_filename, self.new_line_start)
+        old_filespec = "{}:{}".format(
+            self.file_diff.old_file,
+            self.old_line_start)
+        new_filespec = "{}:{}".format(
+            self.file_diff.abs_filename,
+            self.new_line_start)
+        return (old_filespec, new_filespec)
 
     def get_regions(self, view):
         """Create a `sublime.Region` for each part of this hunk."""
