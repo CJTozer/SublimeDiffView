ead5568c5dfa6cc8a8cf0b9ec6a0ed1d1950bcc6
Chris Tozer <chris.tozer@metaswitch.com>
2015-12-19 23:07:25 +0000

    Finish off HunkDiff test

--- tests/test_hunk_diff.py
+++ tests/test_hunk_diff.py
@@ -85,6 +85,105 @@ class test_DiffRegion(TestCase):
              'some_function():',
              '2 | +-'])
 
+    def test_simple_mod_3_context(self):
+        # @@ -116,8 +119,9 @@ some_function():
+        #  unchanged line 1
+        #  unchanged line 2
+        #  unchanged line 3
+        # -old line 1
+        # -old line 2
+        # +new line 1
+        # +new line 2
+        # +new line 3
+        #  unchanged line 4
+        #  unchanged line 5
+        #  unchanged line 6
+        match = ['116', '8', '119', '9',
+            'some_function():\n' +
+            ' unchanged line 1\n' +
+            ' unchanged line 2\n' +
+            ' unchanged line 3\n' +
+            '-old line 1\n' +
+            '-old line 2\n' +
+            '+new line 1\n' +
+            '+new line 2\n' +
+            '+new line 3\n' +
+            ' unchanged line 4\n' +
+            ' unchanged line 5\n' +
+            ' unchanged line 6\n'
+        ]
+        h = HunkDiff(self.file_diff, match)
+        self.assertEqual(len(h.old_regions), 1)
+        self.check_region(
+            h.old_regions[0],
+            'DEL', 119, 0, 121, 0)
+        self.assertEqual(len(h.new_regions), 1)
+        self.check_region(
+            h.new_regions[0],
+            'ADD', 122, 0, 125, 0)
+        self.assertEqual(h.old_line_focus, 119)
+        self.assertEqual(h.new_line_focus, 122)
+        self.assertEqual(h.hunk_type, 'MOD')
+        self.assertEqual(
+            h.description,
+            ['dummy_filename : 122',
+             'some_function():',
+             '5 | +++--'])
+
+    def test_complex_mod_2_context(self):
+        # @@ -116,8 +119,9 @@ some_function():
+        #  unchanged line 1
+        #  unchanged line 2
+        # -old line 1
+        # +new line 1
+        #  unchanged line 3
+        # -old line 2
+        # +new line 2
+        #  unchanged line 4
+        # +new line 3
+        #  unchanged line 5
+        #  unchanged line 6
+        match = ['116', '8', '119', '9',
+            'some_function():\n' +
+            ' unchanged line 1\n' +
+            ' unchanged line 2\n' +
+            '-old line 1\n' +
+            '+new line 1\n' +
+            ' unchanged line 3\n' +
+            '-old line 2\n' +
+            '+new line 2\n' +
+            ' unchanged line 4\n' +
+            '+new line 3\n' +
+            ' unchanged line 5\n' +
+            ' unchanged line 6\n'
+        ]
+        h = HunkDiff(self.file_diff, match)
+        self.assertEqual(len(h.old_regions), 2)
+        self.check_region(
+            h.old_regions[0],
+            'DEL', 118, 0, 119, 0)
+        self.check_region(
+            h.old_regions[1],
+            'DEL', 120, 0, 121, 0)
+        self.assertEqual(len(h.new_regions), 3)
+        self.check_region(
+            h.new_regions[0],
+            'ADD', 121, 0, 122, 0)
+        self.check_region(
+            h.new_regions[1],
+            'ADD', 123, 0, 124, 0)
+        self.check_region(
+            h.new_regions[2],
+            'ADD', 125, 0, 126, 0)
+        self.assertEqual(h.old_line_focus, 118)
+        self.assertEqual(h.new_line_focus, 121)
+        self.assertEqual(h.hunk_type, 'MOD')
+        self.assertEqual(
+            h.description,
+            ['dummy_filename : 121',
+             'some_function():',
+             '5 | +++--'])
+
 
     def check_region(self, r, diff_type, start_line, start_col, end_line, end_col):
         self.assertEqual(r.diff_type, diff_type)
