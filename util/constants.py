905d8826b794e234194c445ed6720fc0b761b4c3
Chris Tozer <christopherjtozer@gmail.com>
2015-12-23 14:40:17 +0000

    Use more standard styles

--- util/constants.py
+++ util/constants.py
@@ -6,9 +6,9 @@ class Constants(object):
     ADD_REGION_KEY = 'diffview-highlight-addition'
     MOD_REGION_KEY = 'diffview-highlight-modification'
     DEL_REGION_KEY = 'diffview-highlight-deletion'
-    ADD_REGION_STYLE = 'support.class'
-    MOD_REGION_STYLE = 'string'
-    DEL_REGION_STYLE = 'invalid'
+    ADD_REGION_STYLE = 'markup.inserted'
+    MOD_REGION_STYLE = 'markup.changed'
+    DEL_REGION_STYLE = 'markup.deleted'
     ADD_REGION_FLAGS = (sublime.DRAW_EMPTY |
                         sublime.HIDE_ON_MINIMAP |
                         sublime.DRAW_EMPTY_AS_OVERWRITE |
